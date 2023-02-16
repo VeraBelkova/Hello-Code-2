411dc4475c0043986889540cb4f950653467eff1
Vera <vera100586@mail.ru>
2023-02-16 17:05:23 +0300

    ex010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 4, 8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  // index = index + 1;
+  index++;
+}
